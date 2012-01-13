a99549d2581e999e7916f932919d6b0e392715b2
PuffPuff <e63.mobile@gmail.com>
2012-01-13 03:28:15 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FairyWhaleDemo::Application.config.secret_token = 'a70d663c224febb03e0288aa54bd934bf6b74c229ff4c293da195d14a45587dc52524fd936eddeb807f69dec0ba40e82a53403d02bf5a1995599eecb9d07dd14'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FairyWhaleDemo::Application.config.session_store :cookie_store, key: '_FairyWhale_demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FairyWhaleDemo::Application.config.session_store :active_record_store
